38cd4b9eb50f272e33080c1dbb7229990e918352
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-07-23 10:26:15 +0700

    chore!: change database engine from sqlite into postgresql

---
+++ src/database/migrations/20240723032314_1_0_0/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "subscriptions" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions_token_key" ON "subscriptions"("token");
