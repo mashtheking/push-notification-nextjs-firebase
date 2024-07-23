d70528cccfbaa6758b517234b1d08a4425fca3cc
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-07-23 16:34:39 +0700

    feat: setup metadata and open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,33 @@ import mui from "@/theme/mui";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://push-notification-nextjs-firebase.vercel.app"),
   title: "NgodingBang Push Notification using Next.js and Firebase",
-  description: "Push Notification using Next.js and Firebase",
+  description: "Create a simple push notification on a web using Next.js and Firebase.",
+  applicationName: "NgodingBang Push Notification",
+  authors: {
+    "name": "Septianata Rizky Pratama",
+    "url": "https://github.com/ngodingbang"
+  },
+  keywords: [
+    "Next.js",
+    "Push Notification",
+    "Firebase Cloud Messaging",
+    "Material UI",
+    "NgodingBang",
+    "Prisma.js",
+    "PostgreSQL",
+    "Vercel"
+  ],
+  publisher: "Vercel",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#4facfe"
 };
 
 export default function Layout({
